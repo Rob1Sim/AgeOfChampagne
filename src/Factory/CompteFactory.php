075e0ede084e8693bcac6433996ac4b192665a9f
Robin Simonneau <robin.simonneau@etudiant.univ-reims.fr>
2022-11-25 17:05:03 +0100

    Ajout de paramètre personalisé pour un compte dans la forge de Compte

--- src/Factory/CompteFactory.php
+++ src/Factory/CompteFactory.php
@@ -4,55 +4,59 @@ namespace App\Factory;
 
 use App\Entity\Compte;
 use App\Repository\CompteRepository;
-use Zenstruck\Foundry\RepositoryProxy;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Zenstruck\Foundry\ModelFactory;
 use Zenstruck\Foundry\Proxy;
+use Zenstruck\Foundry\RepositoryProxy;
 
 /**
  * @extends ModelFactory<Compte>
  *
- * @method static Compte|Proxy createOne(array $attributes = [])
- * @method static Compte[]|Proxy[] createMany(int $number, array|callable $attributes = [])
- * @method static Compte[]|Proxy[] createSequence(array|callable $sequence)
- * @method static Compte|Proxy find(object|array|mixed $criteria)
- * @method static Compte|Proxy findOrCreate(array $attributes)
- * @method static Compte|Proxy first(string $sortedField = 'id')
- * @method static Compte|Proxy last(string $sortedField = 'id')
- * @method static Compte|Proxy random(array $attributes = [])
- * @method static Compte|Proxy randomOrCreate(array $attributes = [])
- * @method static Compte[]|Proxy[] all()
- * @method static Compte[]|Proxy[] findBy(array $attributes)
- * @method static Compte[]|Proxy[] randomSet(int $number, array $attributes = [])
- * @method static Compte[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
+ * @method static Compte|Proxy                     createOne(array $attributes = [])
+ * @method static Compte[]|Proxy[]                 createMany(int $number, array|callable $attributes = [])
+ * @method static Compte[]|Proxy[]                 createSequence(array|callable $sequence)
+ * @method static Compte|Proxy                     find(object|array|mixed $criteria)
+ * @method static Compte|Proxy                     findOrCreate(array $attributes)
+ * @method static Compte|Proxy                     first(string $sortedField = 'id')
+ * @method static Compte|Proxy                     last(string $sortedField = 'id')
+ * @method static Compte|Proxy                     random(array $attributes = [])
+ * @method static Compte|Proxy                     randomOrCreate(array $attributes = [])
+ * @method static Compte[]|Proxy[]                 all()
+ * @method static Compte[]|Proxy[]                 findBy(array $attributes)
+ * @method static Compte[]|Proxy[]                 randomSet(int $number, array $attributes = [])
+ * @method static Compte[]|Proxy[]                 randomRange(int $min, int $max, array $attributes = [])
  * @method static CompteRepository|RepositoryProxy repository()
- * @method Compte|Proxy create(array|callable $attributes = [])
+ * @method        Compte|Proxy                     create(array|callable $attributes = [])
  */
 final class CompteFactory extends ModelFactory
 {
-    public function __construct()
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
     {
         parent::__construct();
 
-        // TODO inject services if required (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services)
+        $this->passwordHasher = $passwordHasher;
     }
 
     protected function getDefaults(): array
     {
         return [
             // TODO add your default values here (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories)
-            'email' => self::faker()->text(),
+            'email' => self::faker()->unique()->numerify('user-####').'@example.fr',
             'roles' => [],
-            'password' => self::faker()->text(),
-            'dateNaiss' => self::faker()->dateTime(),
-            'login' => self::faker()->text(),
+            'password' => 'test',
+            'dateNaiss' => self::faker()->dateTimeBetween(),
+            'login' => self::faker()->firstName(),
         ];
     }
 
     protected function initialize(): self
     {
-        // see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
         return $this
-            // ->afterInstantiate(function(Compte $compte): void {})
+            ->afterInstantiate(function (Compte $user) {
+                $user->setPassword($this->passwordHasher->hashPassword($user, $user->getPassword()));
+            })
         ;
     }
 
