ba94d6a7f0809c5e4f388b0d3c4c18aaa2af0847
breb0007 <noa.brebant@etudiant.univ-reims.fr>
2022-11-29 13:05:25 +0100

    Correction d'un problème d'appel de méthode dans CarteFixtures.php

--- src/DataFixtures/CarteFixtures.php
+++ src/DataFixtures/CarteFixtures.php
@@ -6,14 +6,11 @@ use App\Factory\CarteFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
-use function Zenstruck\Foundry\faker;
-
 class CarteFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
         CarteFactory::createMany(10);
         $manager->flush();
-
     }
 }
