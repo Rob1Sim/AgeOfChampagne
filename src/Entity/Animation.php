f0b101f95274377a275599b734f6ed59a69f761e
Robin Simonneau <robisimonneau@gmail.com>
2022-11-30 11:24:38 +0100

    Création de la class Animation

---
+++ src/Entity/Animation.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AnimationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AnimationRepository::class)]
+class Animation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $horaireDeb = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $horaireFin = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getHoraireDeb(): ?\DateTimeInterface
+    {
+        return $this->horaireDeb;
+    }
+
+    public function setHoraireDeb(\DateTimeInterface $horaireDeb): self
+    {
+        $this->horaireDeb = $horaireDeb;
+
+        return $this;
+    }
+
+    public function getHoraireFin(): ?\DateTimeInterface
+    {
+        return $this->horaireFin;
+    }
+
+    public function setHoraireFin(\DateTimeInterface $horaireFin): self
+    {
+        $this->horaireFin = $horaireFin;
+
+        return $this;
+    }
+}
