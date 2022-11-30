17027cfb388d07e033edadaad74228ea7d6ba2e8
Robin Simonneau <robisimonneau@gmail.com>
2022-11-30 11:30:34 +0100

    Ajout de données factice

---
+++ src/Factory/AnimationFactory.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Animation;
+use App\Repository\AnimationRepository;
+use Zenstruck\Foundry\RepositoryProxy;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+
+/**
+ * @extends ModelFactory<Animation>
+ *
+ * @method static Animation|Proxy createOne(array $attributes = [])
+ * @method static Animation[]|Proxy[] createMany(int $number, array|callable $attributes = [])
+ * @method static Animation[]|Proxy[] createSequence(array|callable $sequence)
+ * @method static Animation|Proxy find(object|array|mixed $criteria)
+ * @method static Animation|Proxy findOrCreate(array $attributes)
+ * @method static Animation|Proxy first(string $sortedField = 'id')
+ * @method static Animation|Proxy last(string $sortedField = 'id')
+ * @method static Animation|Proxy random(array $attributes = [])
+ * @method static Animation|Proxy randomOrCreate(array $attributes = [])
+ * @method static Animation[]|Proxy[] all()
+ * @method static Animation[]|Proxy[] findBy(array $attributes)
+ * @method static Animation[]|Proxy[] randomSet(int $number, array $attributes = [])
+ * @method static Animation[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
+ * @method static AnimationRepository|RepositoryProxy repository()
+ * @method Animation|Proxy create(array|callable $attributes = [])
+ */
+final class AnimationFactory extends ModelFactory
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        // TODO inject services if required (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services)
+    }
+
+    protected function getDefaults(): array
+    {
+        return [
+            // TODO add your default values here (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories)
+            'type' => self::faker()->name(),
+            'nom' => self::faker()->name(),
+            'horaireDeb' => self::faker()->dateTime(),
+            'horaireFin' => self::faker()->dateTime(),
+        ];
+    }
+
+    protected function initialize(): self
+    {
+        // see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+        return $this
+            // ->afterInstantiate(function(Animation $animation): void {})
+        ;
+    }
+
+    protected static function getClass(): string
+    {
+        return Animation::class;
+    }
+}
