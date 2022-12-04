a636fc19e32e2a930373ba4411c16a1abed432c9
Erwann Maton <erwann.maton@etudiant.univ-reims.fr>
2022-12-04 10:45:28 +0100

    Création d'un Cest dédié à l'action logout() du SecurityController

---
+++ tests/Controller/SecurityController/logoutCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Tests\Controller\SecurityController;
+
+use App\Tests\ControllerTester;
+
+class logoutCest
+{
+    public function _before(ControllerTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ControllerTester $I)
+    {
+    }
+}
