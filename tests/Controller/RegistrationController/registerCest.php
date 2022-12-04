3b0d92e56bb5a27310f07f166154946259a2e4b2
Erwann Maton <erwann.maton@etudiant.univ-reims.fr>
2022-12-04 11:00:54 +0100

    Création d'un Cest dédié à l'action register() du RegistrationController

---
+++ tests/Controller/RegistrationController/registerCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Tests\Controller\RegistrationController;
+
+use App\Tests\ControllerTester;
+
+class registerCest
+{
+    public function _before(ControllerTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ControllerTester $I)
+    {
+    }
+}
