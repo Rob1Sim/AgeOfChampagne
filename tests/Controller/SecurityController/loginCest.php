f564c66e83452a4cbea14f9eb44275842b185501
Erwann Maton <erwann.maton@etudiant.univ-reims.fr>
2022-12-07 11:06:41 +0100

    Test login : OK

--- tests/Controller/SecurityController/loginCest.php
+++ tests/Controller/SecurityController/loginCest.php
@@ -2,22 +2,23 @@
 
 namespace App\Tests\Controller\SecurityController;
 
+use App\Factory\CompteFactory;
 use App\Tests\ControllerTester;
 
 class loginCest
 {
     public function _before(ControllerTester $I)
     {
+
     }
 
     // tests
     public function tryToTest(ControllerTester $I)
     {
+        $user = CompteFactory::createOne(['email' => 'erwann.mutant@gmail.com', 'roles' => ['ROLE_ADMIN'], 'password' => 'test',  'login' => 'Erwann', 'isVerified' => true]);
+        $realUser = $user->object();
         $I->amOnPage('/login');
         $I->seeResponseCodeIsSuccessful();
-        $I->fillField('Adresse Mail', 'root@example.fr');
-        $I->fillField('Mot de passe', 'test');
-        $I->click('Connexion');
-        $I->seeCurrentUrlEquals('/admin');
+        $I->amLoggedInAs($realUser);
     }
 }
