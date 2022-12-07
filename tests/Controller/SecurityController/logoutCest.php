8225d1f3bf94ded92cb94a456e38d0c5ed502bc3
Erwann Maton <erwann.maton@etudiant.univ-reims.fr>
2022-12-07 11:16:58 +0100

    Test logout : OK

--- tests/Controller/SecurityController/logoutCest.php
+++ tests/Controller/SecurityController/logoutCest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Controller\SecurityController;
 
+use App\Factory\CompteFactory;
 use App\Tests\ControllerTester;
 
 class logoutCest
@@ -13,5 +14,12 @@ class logoutCest
     // tests
     public function tryToTest(ControllerTester $I)
     {
+        $user = CompteFactory::createOne(['email' => 'roro@gmail.com', 'roles' => ['ROLE_ADMIN'], 'password' => 'test',  'login' => 'Erwann', 'isVerified' => true]);
+        $realUser = $user->object();
+        $I->amOnPage('/login');
+        $I->seeResponseCodeIsSuccessful();
+        $I->amLoggedInAs($realUser);
+        $I->amOnPage('/login');
+        $I->click('Déconnexion');
     }
 }
