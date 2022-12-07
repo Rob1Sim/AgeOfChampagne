a6a44f4f7f6e5b371cb3840d804fd1de4f90cd64
Robin Simonneau <robisimonneau@gmail.com>
2022-12-07 10:16:37 +0100

    Ajout de propriété dans Carte et vignerons

--- src/Entity/Vigneron.php
+++ src/Entity/Vigneron.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\VigneronRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: VigneronRepository::class)]
@@ -27,6 +29,14 @@ class Vigneron
 
     #[ORM\Column(length: 255)]
     private ?string $ville = null;
+
+    #[ORM\OneToMany(mappedBy: 'vignerons', targetEntity: Carte::class)]
+    private Collection $cartes;
+
+    public function __construct()
+    {
+        $this->cartes = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -89,6 +99,36 @@ class Vigneron
     public function setVille(string $ville): self
     {
         $this->ville = $ville;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Carte>
+     */
+    public function getCartes(): Collection
+    {
+        return $this->cartes;
+    }
+
+    public function addCarte(Carte $carte): self
+    {
+        if (!$this->cartes->contains($carte)) {
+            $this->cartes->add($carte);
+            $carte->setVignerons($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCarte(Carte $carte): self
+    {
+        if ($this->cartes->removeElement($carte)) {
+            // set the owning side to null (unless already changed)
+            if ($carte->getVignerons() === $this) {
+                $carte->setVignerons(null);
+            }
+        }
 
         return $this;
     }
