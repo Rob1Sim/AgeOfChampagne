7356d1ab7dd3c1b5b5f941019453e260b0458d98
nonow <noa.brebant@etudiant.univ-reims.fr>
2022-12-08 15:44:10 +0100

    Modifications dans la méthode index afin de s'adapter à la recherche

--- src/Controller/CarteController.php
+++ src/Controller/CarteController.php
@@ -5,15 +5,17 @@ namespace App\Controller;
 use App\Repository\CarteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CarteController extends AbstractController
 {
     #[Route('/carte', name: 'app_carte')]
-    public function index(CarteRepository $repository): Response
+    public function index(Request $request, CarteRepository $repository): Response
     {
-        $contacts = $repository->findAll();
+        $carte = $request->query->get('search', '');
+        $carteList = $repository->search($carte);
 
-        return $this->render('carte/index.html.twig', ['liste' => $contacts]);
+        return $this->render('carte/index.html.twig', ['liste' => $carteList]);
     }
 }
