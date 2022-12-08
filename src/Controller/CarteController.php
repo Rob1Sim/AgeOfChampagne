f634280a4577be844d12cc30e326d8468c4e2e5c
nonow <noa.brebant@etudiant.univ-reims.fr>
2022-12-08 15:48:35 +0100

    Tri des use par ordre alphabétique pour être conforme PSR-12

--- src/Controller/CarteController.php
+++ src/Controller/CarteController.php
@@ -4,8 +4,8 @@ namespace App\Controller;
 
 use App\Repository\CarteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CarteController extends AbstractController
