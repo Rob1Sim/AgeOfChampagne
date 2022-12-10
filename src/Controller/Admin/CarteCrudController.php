4ecd0fc9980623131fee29928ca81c56a7a1b743
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2022-12-10 20:00:52 +0100

    Bug : Impossible d'upload une image si on vérifie son type

--- src/Controller/Admin/CarteCrudController.php
+++ src/Controller/Admin/CarteCrudController.php
@@ -39,8 +39,15 @@ class CarteCrudController extends AbstractCrudController
                     return $entity->getVignerons()->getNom().' '.$entity->getVignerons()->getPrenom();
                 }),
             ImageField::new('contenuImage', 'Image de la carte')
+                ->setBasePath('uploads/image/')
                 ->setUploadDir('public/uploads/img/')
                 ->setUploadedFileNamePattern('[slug]-[timestamp].[extension]')
+                /**->setFormTypeOption('constraints', [
+                    new \Symfony\Component\Validator\Constraints\Image([
+                        'maxSize' => '5M',
+                        'mimeTypesMessage' => "Le type d'image est invalide",
+                    ]),
+                ])**/
                 ->onlyOnForms(),
         ];
     }
