98031db299180a44c92114d17a461c69ae936e58
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2022-12-10 12:12:44 +0100

    Champs de mots de passe caché, et changement du nom des champs de comptes

--- src/Controller/Admin/CompteCrudController.php
+++ src/Controller/Admin/CompteCrudController.php
@@ -4,6 +4,12 @@ namespace App\Controller\Admin;
 
 use App\Entity\Compte;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class CompteCrudController extends AbstractCrudController
 {
@@ -12,14 +18,16 @@ class CompteCrudController extends AbstractCrudController
         return Compte::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            EmailField::new('email', 'Email'),
+            TextField::new('login', 'Login'),
+            TextField::new('password', 'Mot de passe')->hideOnForm()->hideOnIndex(),
+            DateField::new('datenaiss', 'Date de naissance'),
+            ArrayField::new('roles', 'Roles'),
+            BooleanField::new('is_verified', 'Est vérifié'),
         ];
     }
-    */
 }
