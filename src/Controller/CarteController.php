efc3b057f5f1e7108f17628eda50dca90ee94826
nonow <noa.brebant@etudiant.univ-reims.fr>
2022-12-11 16:51:45 +0100

    Ajout de la méthode show pour afficher une carte selon son id

--- src/Controller/CarteController.php
+++ src/Controller/CarteController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Carte;
 use App\Repository\CarteRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,5 +19,12 @@ class CarteController extends AbstractController
         $carteList = $repository->search($carte);
 
         return $this->render('carte/index.html.twig', ['liste' => $carteList]);
+    }
+
+    #[Route('carte/{id}', requirements: ['id' => '\d+'])]
+    #[Entity('carte', expr: 'repository.findWithCategory(id)')]
+    public function show(Carte $carte): Response
+    {
+        return $this->render('carte/show.html.twig', ['carte' => $carte]);
     }
 }
