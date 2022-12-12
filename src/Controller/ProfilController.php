883906127451fe03d6d502c296fa39458cd365d6
Erwann Maton <erwann.maton@etudiant.univ-reims.fr>
2022-12-12 15:12:59 +0100

    ajout de la fonction permertant de voir le profil d'un compte

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\CompteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfilController extends AbstractController
 {
-    #[Route('/profil', name: 'app_profil')]
-    public function index(): Response
+    #[Route('/profil/{id<\d+>}', name: 'app_profil')]
+    public function index(int $id, CompteRepository $repository): Response
     {
+        $compte = $repository->search($id);
+
+
         return $this->render('profil/index.html.twig', [
-            'controller_name' => 'ProfilController',
+            'compte' => $compte[0],
         ]);
     }
 }
