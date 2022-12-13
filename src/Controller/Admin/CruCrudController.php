cf5753f838fbdc45bc51b6fc8829b2fa6af2bcf1
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2022-12-13 18:50:06 +0100

    Ajout de la relation cru vignerons dans l'admin

--- src/Controller/Admin/CruCrudController.php
+++ src/Controller/Admin/CruCrudController.php
@@ -3,7 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Cru;
+use Doctrine\ORM\EntityRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class CruCrudController extends AbstractCrudController
 {
@@ -12,14 +16,23 @@ class CruCrudController extends AbstractCrudController
         return Cru::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('libelle'),
+            TextField::new('infos'),
+            AssociationField::new('vignerons', 'Vignerons')->setFormTypeOption('choice_label', function ($vigneron) {
+                return $vigneron->getNomPrenom();
+            })
+                ->setFormTypeOption('query_builder', function (EntityRepository $ep) {
+                    return $ep->createQueryBuilder('c')->orderBy('c.nom', 'ASC');
+                })
+                ->formatValue(function ($value, $entity) {
+                    return $entity->getVignerons()->getNom().' '.$entity->getVignerons()->getPrenom();
+                }),
         ];
     }
-    */
+
 }
