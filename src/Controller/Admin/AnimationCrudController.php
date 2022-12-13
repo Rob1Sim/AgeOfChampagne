ea550630c8d816f508185d1424a6f6cd5f1f45ed
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2022-12-13 18:34:47 +0100

    Ajout de la relation entre vignerons et animations dans les champs admins

--- src/Controller/Admin/AnimationCrudController.php
+++ src/Controller/Admin/AnimationCrudController.php
@@ -3,7 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Animation;
+use Doctrine\ORM\EntityRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class AnimationCrudController extends AbstractCrudController
 {
@@ -12,14 +18,24 @@ class AnimationCrudController extends AbstractCrudController
         return Animation::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('type'),
+            DateField::new('horaireDeb'),
+            DateField::new('horaireFin'),
+            MoneyField::new('prix'),
+            AssociationField::new('vignerons')->setFormTypeOption('choice_label', function ($vigneron) {
+                return $vigneron->getNomPrenom();
+            })
+                ->setFormTypeOption('query_builder', function (EntityRepository $ep) {
+                    return $ep->createQueryBuilder('c')->orderBy('c.nom', 'ASC');
+                })
+                ->formatValue(function ($value, $entity) {
+                    return $entity->getVignerons()->getNom().' '.$entity->getVignerons()->getPrenom();
+                }),
         ];
     }
-    */
+
 }
