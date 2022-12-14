57a4b28e0f9357ce7b141967ed2d5c31a36fad52
breb0007 <noa.brebant@etudiant.univ-reims.fr>
2022-12-14 10:50:24 +0100

    Création d'un fichier de tests pour l'affichage d'une seule carte

---
+++ tests/Controller/Carte/ShowCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Tests\Controller\Carte;
+
+use App\Tests\ControllerTester;
+
+class ShowCest
+{
+    public function _before(ControllerTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ControllerTester $I)
+    {
+    }
+}
