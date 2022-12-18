c628a71f0d21d404b9806708d23dde44da728ab5
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2022-12-18 18:46:07 +0100

    Ajout d'un fichier JS qui s'occupe de changer le type que l'input accepte de base (aucun) à des images, ainsi qu'une vérification sur le serveur du type mime du fichier envoyer afin que la requête ne soit pas enregistrer

---
+++ public/js/form.js
@@ -0,0 +1,4 @@
+window.onload = ()=>{
+    console.log(document.getElementById("Carte_contenuImage_file"))
+    document.getElementById("Carte_contenuImage_file").accept = "image/png, image/jpeg"
+}
