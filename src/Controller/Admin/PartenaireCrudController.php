efbfaf850224ff971fda1ac9685a44317efa7e37
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2022-12-19 18:47:16 +0100

    Ajout des champs de données pour renseigné un partenaire

--- src/Controller/Admin/PartenaireCrudController.php
+++ src/Controller/Admin/PartenaireCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Partenaire;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class PartenaireCrudController extends AbstractCrudController
 {
@@ -12,14 +15,13 @@ class PartenaireCrudController extends AbstractCrudController
         return Partenaire::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('nom', 'Nom'),
+            TextField::new('prenom', 'Prénom'),
+            AssociationField::new('vigneronsPart', 'Vignerons partenaires'),
         ];
     }
-    */
 }
