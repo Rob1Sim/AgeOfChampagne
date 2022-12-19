879abd5504e900ab3f50cbb207dd3c40f22d6122
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2022-12-19 18:58:45 +0100

    Formatage des données pour l'affichages de vignerons : BUG : Les vignerons choisie ne sont pas enregistré

--- src/Controller/Admin/PartenaireCrudController.php
+++ src/Controller/Admin/PartenaireCrudController.php
@@ -17,11 +17,27 @@ class PartenaireCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
+        // TODO régler le bug de save
         return [
             IdField::new('id')->hideOnForm(),
             TextField::new('nom', 'Nom'),
             TextField::new('prenom', 'Prénom'),
-            AssociationField::new('vigneronsPart', 'Vignerons partenaires'),
+            AssociationField::new('vigneronsPart', 'Vignerons partenaires')
+                ->setFormTypeOption('choice_label', function ($vigneron) {
+                    return $vigneron->getNom().' '.$vigneron->getPrenom();
+                })
+                ->formatValue(function ($value, $entity) {
+                    if (null != $entity->getVigneronsPart()[0]) {
+                        // Affiche ... si il y a plus d'un vignerons
+                        if (count($entity->getVigneronsPart()) > 1) {
+                            return $entity->getVigneronsPart()[0]->getNom().' '.$entity->getVigneronsPart()[0]->getPrenom().'...';
+                        }
+
+                        return $entity->getVigneronsPart()[0]->getNom().' '.$entity->getVigneronsPart()[0]->getPrenom();
+                    }
+
+                    return 'Pas de vignerons';
+                }),
         ];
     }
 }
