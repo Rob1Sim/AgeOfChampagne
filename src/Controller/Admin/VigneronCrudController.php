fc7c3512883a47019b20990ba596825da277f31d
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2022-12-19 18:10:25 +0100

    Ajout des donénes de formulaire de vignerons

--- src/Controller/Admin/VigneronCrudController.php
+++ src/Controller/Admin/VigneronCrudController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\Vigneron;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class VigneronCrudController extends AbstractCrudController
 {
@@ -12,14 +14,15 @@ class VigneronCrudController extends AbstractCrudController
         return Vigneron::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('nom', 'Nom'),
+            TextField::new('prenom', 'Prénom'),
+            TextField::new('adresse', 'Adresse'),
+            TextField::new('code_postal', 'Code postal'),
+            TextField::new('ville', 'Ville'),
         ];
     }
-    */
 }
