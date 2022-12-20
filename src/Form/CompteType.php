b427496283092e748ecef071af519807b4fa572c
Erwann Maton <erwann.maton@etudiant.univ-reims.fr>
2022-12-20 22:50:11 +0100

    ajout des différents asserts dans le compte et modif du formulaire

--- src/Form/CompteType.php
+++ src/Form/CompteType.php
@@ -4,20 +4,27 @@ namespace App\Form;
 
 use App\Entity\Compte;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class CompteType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('dateNaiss')
-            ->add('login')
-            ->add('is_verified')
+            ->add('login', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('date_naiss', DateType::class, [
+                'widget' => 'single_text',
+            ])
         ;
     }
 
