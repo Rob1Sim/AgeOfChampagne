cd97083af2cbca4f249f25671ce981e5f3dae506
Roman Szczepaniak <roman.szczepaniak@etudiant.univ-reims.fr>
2023-01-07 16:10:28 +0100

    Relation ManyToMany pour Vigneron/Animation et Partenaire OK sans données

--- src/DataFixtures/VigneronFixtures.php
+++ src/DataFixtures/VigneronFixtures.php
@@ -4,9 +4,8 @@ namespace App\DataFixtures;
 
 use App\Entity\Animation;
 use App\Entity\Partenaire;
-use App\Factory\AnimationFactory;
+use App\Entity\Vigneron;
 use App\Factory\CruFactory;
-use App\Factory\PartenaireFactory;
 use App\Factory\ProduitFactory;
 use App\Factory\VigneronFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -19,6 +18,8 @@ class VigneronFixtures extends Fixture implements DependentFixtureInterface
     {
         $animation = new Animation();
         $partenaire = new Partenaire();
+        $vigneron = new Vigneron();
+
         $vignerons = VigneronFactory::createMany(10, function () {
             return [
                 'cru' => CruFactory::random(),
@@ -27,10 +28,20 @@ class VigneronFixtures extends Fixture implements DependentFixtureInterface
         });
         foreach ($vignerons as $relations) {
             return [
+                $this->addReference('vigneron_animation', $animation),
+                $this->addReference('vigneron_partenaire', $partenaire),
+
                 $relations->addAnimation($animation),
+                $animation->addVigneronsAnim($vigneron),
+
                 $relations->addPartenaire($partenaire),
+                $partenaire->addVigneronsPart($vigneron),
             ];
         }
+        $manager->persist($partenaire);
+        $manager->persist($vigneron);
+        $manager->persist($animation);
+        $manager->flush();
     }
 
     public function getDependencies(): array
