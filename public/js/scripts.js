9cc9829b5727dc9f8f4417e9d4d4282050fda020
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2023-01-07 18:04:30 +0100

    Ajout du menu (dropdown) de sélection du language

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,15 +1,49 @@
-
-
-
-
-
+/**
+ * Fonction qui s'occupe d'afficher un dropDown pour choisir sa langue
+ */
 function dropdownFlags(){
     const flagDropdown = document.querySelector('div.dropdown-flags');
     const flagButton = document.querySelector('button.btn-flag');
+    flagDropdown.style.display = "none";
     flagButton.addEventListener('click',()=>{
-        flagDropdown.classList.toggle("show");
+
+        if ( flagDropdown.style.display === "none" ){
+            flagDropdown.style.display = "block";
+        }else{
+            flagDropdown.style.display = "none";
+        }
+        //flagDropdown.classList.toggle("show");
 
     })
+
+    const frFlagButton = document.querySelector('button.btn-flag-fr');
+    const usFlagButton = document.querySelector('button.btn-flag-us');
+
+    frFlagButton.addEventListener('click',()=>{
+        let currentFlag = document.querySelector('img.btn-img');
+        currentFlag.src = "image/flags/france.png";
+        flagDropdown.style.display = "none";
+
+    })
+
+    usFlagButton.addEventListener('click',()=>{
+        let currentFlag = document.querySelector('img.btn-img');
+        currentFlag.src = "image/flags/united-states.png";
+        flagDropdown.style.display = "none";
+
+    })
+
 }
 
 dropdownFlags();
+
+// Fereme le dropdown si on clique autre part que sur le bouton
+window.onclick = function(event) {
+    if (!event.target.matches('.btn-flag') && !event.target.matches('.btn-img')&& !event.target.matches('.btn-flag-fr')
+        && !event.target.matches('.btn-img-fr') && !event.target.matches('.btn-flag-fr') && !event.target.matches('.btn-img-fr')) {
+        const flagDropdown = document.querySelector('div.dropdown-flags');
+        flagDropdown.style.display = "none";
+
+
+    }
+}
