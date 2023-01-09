8a2a6cedfc25d7ea107ea196e4a59b45f4b6de12
Erwann Maton <erwann.maton@etudiant.univ-reims.fr>
2023-01-09 22:34:14 +0100

    Mise en place du mail à envoyer

--- src/Security/EmailVerifier.php
+++ src/Security/EmailVerifier.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Security\Core\User\UserInterface;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
@@ -19,21 +20,17 @@ class EmailVerifier
     ) {
     }
 
-    public function sendEmailConfirmation(string $verifyEmailRouteName, UserInterface $user, TemplatedEmail $email): void
+    public function sendEmailConfirmation(string $email, string $login): void
     {
-        $signatureComponents = $this->verifyEmailHelper->generateSignature(
-            $verifyEmailRouteName,
-            $user->getId(),
-            $user->getEmail()
-        );
-
-        $context = $email->getContext();
-        $context['signedUrl'] = $signatureComponents->getSignedUrl();
-        $context['expiresAtMessageKey'] = $signatureComponents->getExpirationMessageKey();
-        $context['expiresAtMessageData'] = $signatureComponents->getExpirationMessageData();
-
-        $email->context($context);
-
+        $email = (new Email())
+            ->from('contact@oldhengames.com')
+            ->to($email)
+            ->subject('Email de vérification')
+            ->context([
+                'expiration_date' => new \DateTime('+7 days'),
+                'username' => $login,
+            ])
+            ->text('Vérifie ton email stp');
         $this->mailer->send($email);
     }
 
