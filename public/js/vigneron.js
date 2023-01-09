83fa82c7ff9c6b1ca5ff869152828ff4f91b7301
nonow <noa.brebant@etudiant.univ-reims.fr>
2023-01-09 22:10:55 +0100

    Suppression des console.log dans vigneron.js

--- public/js/vigneron.js
+++ public/js/vigneron.js
@@ -18,14 +18,12 @@ input.addEventListener("change",(event)=>{
     const image = document.getElementById("Vigneron_contenuImage_file").files[0];
 
     if (image.type !== "image/png" && image.type !== "image/jpeg"){
-        console.log("Appelé")
         document.getElementsByClassName("flex-fill")[8].innerHTML +=
             "<span style='color: red' id='error-message'>Le type du fichier n'est pas accepté</span>";
         // On désactive les boutons de submissions
         document.querySelectorAll("button[type='submit']")[0].disabled = true;
         document.querySelectorAll("button[type='submit']")[1].disabled = true;
     }else{
-        console.log("Reappelé")
         document.getElementById("error-message").innerHTML =""
         // On active les boutons de submissions
         document.querySelectorAll("button[type='submit']")[0].disabled = false;
