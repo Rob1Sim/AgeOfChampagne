8df99e0f65bd09513fdee3bdce8112a86acc9f1b
breb0007 <noa.brebant@etudiant.univ-reims.fr>
2023-01-10 17:40:49 +0100

    Création du fichier test pour les crus

---
+++ tests/Controller/Cru/ShowCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Tests\Controller\Cru;
+
+use App\Tests\ControllerTester;
+
+class ShowCest
+{
+    public function _before(ControllerTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ControllerTester $I)
+    {
+    }
+}
