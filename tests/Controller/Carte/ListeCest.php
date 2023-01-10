c865c41ba7fb9183c18619abc0da56fa33159d1e
breb0007 <noa.brebant@etudiant.univ-reims.fr>
2023-01-10 16:49:48 +0100

    Mise à jour des tests : Ajout de catégories aux cartes

--- tests/Controller/Carte/ListeCest.php
+++ tests/Controller/Carte/ListeCest.php
@@ -5,14 +5,15 @@ namespace App\Tests\Controller\Carte;
 
 use App\Entity\Carte;
 use App\Factory\CarteFactory;
+use App\Factory\CategoryFactory;
 use App\Tests\ControllerTester;
 
 class ListeCest
 {
     public function listeCartes(ControllerTester $I)
     {
-        CarteFactory::createMany(10);
-        CarteFactory::createOne(['nom' => 'Aaaaaaaaaaaa']);
+        CarteFactory::createMany(10, ['category' => CategoryFactory::createOne()]);
+        CarteFactory::createOne(['nom' => 'Aaaaaaaaaaaa', 'category' => CategoryFactory::createOne()]);
         $I->amOnPage('/carte');
         $I->seeResponseCodeIsSuccessful();
         $I->seeInTitle('Liste des cartes');
@@ -22,8 +23,8 @@ class ListeCest
 
     public function redirectToCarte(ControllerTester $I)
     {
-        CarteFactory::createMany(5);
-        CarteFactory::createOne(['nom' => 'Aaaaaaaaaaaa']);
+        CarteFactory::createMany(5, ['category' => CategoryFactory::createOne()]);
+        CarteFactory::createOne(['nom' => 'Aaaaaaaaaaaa', 'category' => CategoryFactory::createOne()]);
         $I->amOnPage('/carte');
         $I->seeResponseCodeIsSuccessful();
         $I->click('Aaaaaaaaaaaa');
