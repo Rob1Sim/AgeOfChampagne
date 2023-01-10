ac4e95ae65ce6c7a193d4c212138e45b96ad8c74
breb0007 <noa.brebant@etudiant.univ-reims.fr>
2023-01-10 17:10:20 +0100

    Mise à jour des tests : Connexion à un compte admin

--- tests/Controller/Carte/ListeCest.php
+++ tests/Controller/Carte/ListeCest.php
@@ -6,12 +6,17 @@ namespace App\Tests\Controller\Carte;
 use App\Entity\Carte;
 use App\Factory\CarteFactory;
 use App\Factory\CategoryFactory;
+use App\Factory\CompteFactory;
 use App\Tests\ControllerTester;
 
 class ListeCest
 {
     public function listeCartes(ControllerTester $I)
     {
+        $user = CompteFactory::createOne(['roles' => ['ROLE_ADMIN']]);
+        $trueUser = $user->object();
+        $I->amLoggedInAs($trueUser);
+
         CarteFactory::createMany(10, ['category' => CategoryFactory::createOne()]);
         CarteFactory::createOne(['nom' => 'Aaaaaaaaaaaa', 'category' => CategoryFactory::createOne()]);
         $I->amOnPage('/carte');
@@ -23,6 +28,10 @@ class ListeCest
 
     public function redirectToCarte(ControllerTester $I)
     {
+        $user = CompteFactory::createOne(['roles' => ['ROLE_ADMIN']]);
+        $trueUser = $user->object();
+        $I->amLoggedInAs($trueUser);
+
         CarteFactory::createMany(5, ['category' => CategoryFactory::createOne()]);
         CarteFactory::createOne(['nom' => 'Aaaaaaaaaaaa', 'category' => CategoryFactory::createOne()]);
         $I->amOnPage('/carte');
