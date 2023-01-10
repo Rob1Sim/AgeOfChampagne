130b5c85e1438d3111aa6795e75e5ec007d95d73
breb0007 <noa.brebant@etudiant.univ-reims.fr>
2023-01-10 17:10:53 +0100

    Mise à jour des tests : Connexion à un compte admin + Ajout du fait que les cartes récemment visitées comptent

--- tests/Controller/Carte/SearchCest.php
+++ tests/Controller/Carte/SearchCest.php
@@ -1,16 +1,20 @@
 <?php
 
-
 namespace App\Tests\Controller\Carte;
 
 use App\Factory\CarteFactory;
 use App\Factory\CategoryFactory;
+use App\Factory\CompteFactory;
 use App\Tests\ControllerTester;
 
 class SearchCest
 {
     public function search(ControllerTester $I)
     {
+        $user = CompteFactory::createOne(['roles' => ['ROLE_ADMIN']]);
+        $trueUser = $user->object();
+        $I->amLoggedInAs($trueUser);
+
         /*
          * Création de 5 cartes dont :
          * - 3 contiennent un xxxxx
@@ -24,12 +28,14 @@ class SearchCest
         CarteFactory::createOne(['nom' => 'xxxxxyyyyy', 'category' => CategoryFactory::createOne()]);
         CarteFactory::createOne(['nom' => 'xxxxxyyyyyzzzzz', 'category' => CategoryFactory::createOne()]);
         $I->amOnPage('/carte?search=zzzzz');
-        $I->seeNumberOfElements('.carte', 2);
+
+        // +1 car le test inclut également les cartes récemment visitées
+        $I->seeNumberOfElements('.carte', 2 + 1);
         $I->amOnPage('/carte?search=yyyyy');
-        $I->seeNumberOfElements('.carte', 3);
+        $I->seeNumberOfElements('.carte', 3 + 1);
         $I->amOnPage('/carte?search=xxxxx');
-        $I->seeNumberOfElements('.carte', 3);
+        $I->seeNumberOfElements('.carte', 3 + 1);
         $I->amOnPage('/carte?search=ooooo');
-        $I->seeNumberOfElements('.carte', 0);
+        $I->seeNumberOfElements('.carte', 0 + 1);
     }
 }
