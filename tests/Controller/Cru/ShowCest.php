5da52bcb13502b709e12bb8a20bfc07bfe52272f
breb0007 <noa.brebant@etudiant.univ-reims.fr>
2023-01-10 17:43:26 +0100

    Ajout de la méthode de tests testCruShow

--- tests/Controller/Cru/ShowCest.php
+++ tests/Controller/Cru/ShowCest.php
@@ -3,16 +3,21 @@
 
 namespace App\Tests\Controller\Cru;
 
+use App\Factory\CompteFactory;
+use App\Factory\CruFactory;
 use App\Tests\ControllerTester;
 
 class ShowCest
 {
-    public function _before(ControllerTester $I)
+    public function testCruShow(ControllerTester $I)
     {
-    }
+        $user = CompteFactory::createOne(['roles' => ['ROLE_ADMIN']]);
+        $trueUser = $user->object();
+        $I->amLoggedInAs($trueUser);
 
-    // tests
-    public function tryToTest(ControllerTester $I)
-    {
+        CruFactory::createOne(['libelle' => 'Szczpeniak']);
+
+        $I->amOnPage('/cru/1');
+        $I->seeCurrentRouteIs('app_cru_show');
     }
 }
