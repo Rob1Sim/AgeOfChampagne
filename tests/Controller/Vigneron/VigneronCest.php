32abf34d2e7edf9ea3d4662587c73e64678abb81
breb0007 <noa.brebant@etudiant.univ-reims.fr>
2023-01-10 17:25:18 +0100

    Ajout d'un test restrited sur Vigneron pour vérifier si nous sommes redirigés vers login quand non connecté

--- tests/Controller/Vigneron/VigneronCest.php
+++ tests/Controller/Vigneron/VigneronCest.php
@@ -3,12 +3,12 @@
 namespace App\Tests\Controller\Vigneron;
 
 use App\Factory\CompteFactory;
+use App\Factory\VigneronFactory;
 use App\Tests\ControllerTester;
-
 
 class VigneronCest
 {
-    public function testVigneronList (ControllerTester $V)
+    public function testVigneronList(ControllerTester $V)
     {
         $user = CompteFactory::createOne(['roles' => ['ROLE_ADMIN']]);
         $trueUser = $user->object();
@@ -18,5 +18,13 @@ class VigneronCest
         $V->seeResponseCodeIsSuccessful();
         $V->seeInTitle('Liste des Vignerons');
         $V->see('Liste des Vignerons');
+    }
+
+    // Vérifie que nous sommes bien redirigés sur /login quand nous ne sommes pas connectés !
+    public function restricted(ControllerTester $I)
+    {
+        VigneronFactory::createOne();
+        $I->amOnPage('/vigneron/1');
+        $I->seeCurrentUrlEquals('/login');
     }
 }
